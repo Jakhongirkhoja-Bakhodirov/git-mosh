c7ea0706167e8fa3424f0385ec3b54ec5bec62d6
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-26 16:31:06 +0500

    added person consturctor

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const person = {
-    first_name,
-    last_name
+let person = {
+    first_name:'first_name',
+    last_name:'last_name',
 }
+
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+
+    this.getFullName = function () {
+        return this.firstName + " " + this.lastName;
+    };
+}
+
+let person1 = new Person('Jane','Doe')
+
+console.log(person1.getFullName());
